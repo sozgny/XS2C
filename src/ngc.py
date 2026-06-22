7e682df8904d70ea69498347167d346d6091d11c
sozg <114666012+sozgny@users.noreply.github.com>
2026-06-22 11:19:16 +0200

    add class MLP

--- src/ngc.py
+++ src/ngc.py
@@ -69,7 +69,31 @@ def regularize(network, lam, penalty):
 def ridge_regularize(network, lam):
     '''Apply ridge penalty at all subsequent layers.'''
     return lam * sum([torch.sum(fc.weight ** 2) for fc in network.layers[1:]])
-   
+
+class MLP(nn.Module):
+    def __init__(self, num_series, lag, hidden, activation):
+        super(MLP, self).__init__()
+        self.activation = activation_helper(activation)
+
+        # Set up network.
+        layer = nn.Conv1d(num_series, hidden[0], lag)
+        modules = [layer]
+
+        for d_in, d_out in zip(hidden, hidden[1:] + [1]):
+            layer = nn.Conv1d(d_in, d_out, 1)
+            modules.append(layer)
+
+        # Register parameters.
+        self.layers = nn.ModuleList(modules)
+
+    def forward(self, X):
+        X = X.transpose(2, 1)
+        for i, fc in enumerate(self.layers):
+            if i != 0:
+                X = self.activation(X)
+            X = fc(X)
+
+        return X.transpose(2, 1)
     
 class cMLP(nn.Module):
     def __init__(self, num_series, lag, hidden, activation='relu'):
